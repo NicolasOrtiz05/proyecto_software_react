570d303690f3fcfd003aa61ac7577cc323d1067a
juan david sanchez <Juansangon@unisabana.edu.co>
2024-11-17 12:19:15 -0500

    pruebas cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,29 @@
-// cypress.config.js
+// cypress.ci.config.js
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome') {
+          launchOptions.args.push('--disable-dev-shm-usage');
+          return launchOptions;
+        }
+      });
     },
     viewportWidth: 1382,
     viewportHeight: 736,
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 30000,
-    // Configuración para screenshots y videos
-    screenshotOnRunFailure: true,
-    video: true,
-    videosFolder: "cypress/videos",
-    screenshotsFolder: "cypress/screenshots",
+    testIsolation: false,
+    experimentalRunAllSpecs: true,
+    retries: {
+      runMode: 2,
+      openMode: 0
+    },
+    // Desactivar el fallo de las pruebas por errores
+    experimentalWebKitSupport: true,
+    chromeWebSecurity: false
   },
 });
