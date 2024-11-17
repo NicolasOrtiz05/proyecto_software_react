50bae9c7b931ec7460bc9c2b5c6bf55c4e0e47b5
juan david sanchez <Juansangon@unisabana.edu.co>
2024-11-17 11:18:04 -0500

    prueba cypress 33

--- cypress/e2e/pagprincipal.cy.js
+++ cypress/e2e/pagprincipal.cy.js
@@ -1,35 +1,59 @@
 describe('paginaprincipaltest', function () {
-  const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
-
-  it('paginaprincipaltest', function () {
-    // Visita la página principal
-    cy.visit(BASE_URL);
-
+  beforeEach(function() {
+    // Visitar la página principal antes de cada prueba
+    cy.visit('/');
+    
+    // Esperar a que los productos se carguen inicialmente
+    cy.get('.producto', { timeout: 10000 }).should('exist');
+    
     // Establecer el tamaño de la ventana
     cy.viewport(1382, 736);
+  });
 
-    // Realizar clic en productos
-    clickWhenVisible(".producto:nth-child(1) .producto-agregar");
-    clickWhenVisible(".producto:nth-child(3) .producto-agregar");
-    clickWhenVisible(".producto:nth-child(11) .producto-agregar");
-    clickWhenVisible(".producto:nth-child(14) .producto-agregar");
+  it('paginaprincipaltest', function () {
+    // Verificar que los productos se han cargado antes de empezar
+    cy.get('.producto').should('have.length.gt', 0).then(() => {
+      // Realizar clic en productos de la página principal
+      clickWhenVisible(".producto:nth-child(1) .producto-agregar");
+      clickWhenVisible(".producto:nth-child(3) .producto-agregar");
+      clickWhenVisible(".producto:nth-child(11) .producto-agregar");
+      clickWhenVisible(".producto:nth-child(14) .producto-agregar");
+    });
 
-    // Navegar entre secciones del menú
-    clickWhenVisible("li:nth-child(2) > .boton-menu");
-    clickWhenVisible(".producto:nth-child(2) .producto-agregar");
+    // Navegar a la segunda sección y agregar producto
+    cy.get('li:nth-child(2) > .boton-menu')
+      .click()
+      .then(() => {
+        cy.get('.producto').should('exist');
+        clickWhenVisible(".producto:nth-child(2) .producto-agregar");
+      });
 
-    clickWhenVisible("li:nth-child(3) > .boton-menu");
-    clickWhenVisible(".producto:nth-child(5) .producto-agregar");
+    // Navegar a la tercera sección y agregar producto
+    cy.get('li:nth-child(3) > .boton-menu')
+      .click()
+      .then(() => {
+        cy.get('.producto').should('exist');
+        clickWhenVisible(".producto:nth-child(5) .producto-agregar");
+      });
 
-    clickWhenVisible("li:nth-child(4) > .boton-menu");
-    clickWhenVisible(".producto:nth-child(3) .producto-agregar");
+    // Navegar a la cuarta sección y agregar producto
+    cy.get('li:nth-child(4) > .boton-menu')
+      .click()
+      .then(() => {
+        cy.get('.producto').should('exist');
+        clickWhenVisible(".producto:nth-child(3) .producto-agregar");
+      });
   });
 });
 
-// Función auxiliar para hacer clic cuando el elemento esté visible
+// Función auxiliar mejorada para hacer clic cuando el elemento esté visible
 const clickWhenVisible = (selector) => {
-  cy.get(selector, { timeout: 60000 })  // Espera hasta 60 segundos para que el elemento sea visible
-    .should('be.visible')               // Espera a que el elemento sea visible
-    .scrollIntoView()                   // Desplaza el elemento al centro de la pantalla
-    .click({ force: true });             // Forza el clic en el elemento
+  cy.get(selector, { timeout: 30000 }) // Reducido a 30 segundos, que debería ser suficiente
+    .should('be.visible')
+    .scrollIntoView({ behavior: 'smooth' })
+    .wait(500) // Esperar a que termine el scroll
+    .then(() => {
+      // Volver a consultar el elemento después del scroll
+      cy.get(selector).should('be.visible').click({ force: true });
+    });
 };
